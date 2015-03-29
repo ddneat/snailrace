cd344019a53cf8170afd7f18a885539e8a596316
David Neubauer <davidneub@gmail.com>
2015-03-29 19:41:20 +0200

    moved cameras into renderer and added callbacks instead of publish/subscribe

--- src/modules/3d/Renderer.js
+++ src/modules/3d/Renderer.js
@@ -3,20 +3,25 @@ export class Renderer {
      * constructor
      * e.g.: new Renderer()
      */
-    constructor(scene) {
+    constructor(game, scene) {
+        this.game = game;
         this.scene = scene;
 
+        this.addInGameCamera();
+        this.addFinishCamera();
+        this.addMultipleViewports();
+
+        this.addRenderer();
         this.addPointLight();
         this.addDirectionalLight();
-        this.init();
+        this.setRenderTarget();
     }
     /**
-     * Renderer.init
-     * e.g.: Renderer.init();
+     * Renderer.addRenderer
+     * e.g.: Renderer.addRenderer();
      */
-    init () {
+    addRenderer () {
         this.webglRenderer = new THREE.WebGLRenderer( {antialias: true, clearColor: 0xc1e9e4, clearAlpha: 1 } );
-
         this.webglRenderer.setSize(window.innerWidth, window.innerHeight);
         this.webglRenderer.sortElements = false;
 
@@ -24,6 +29,68 @@ export class Renderer {
         this.webglRenderer.shadowMapSoft = true;
         this.webglRenderer.shadowMapType = THREE.PCFSoftShadowMap;
         this.webglRenderer.physicallyBasedShading = true;
+
+        THREEx.WindowResize(this.webglRenderer, this.camera);
+    }
+    /**
+     * Renderer.addInGameCamera
+     * e.g.: Renderer.addInGameCamera();
+     */
+    addInGameCamera() {
+        this.camera = new THREE.PerspectiveCamera(45,  window.innerWidth / window.innerHeight, 0.1, 100000);
+        this.camera.position.set(10, 10, 10);
+        this.camera.lastPosition = new THREE.Vector3(10,10,10);
+        this.camera.lookAt(new THREE.Vector3(0,0,0));
+        this.scene.add(this.camera);
+    }
+    /**
+     * Renderer.updateInGameCamera
+     * e.g.: Renderer.updateInGameCamera();
+     *
+     * @param lookAtZ {Number}
+     */
+    updateInGameCamera(lookAtZ) {
+        this.camera.target = new THREE.Vector3(0,0,-lookAtZ);
+        this.camera.lastPosition = this.camera.position;
+        this.camera.lookAt(this.camera.target);
+        this.camera.translateX(lookAtZ / 120);
+    }
+    /**
+     * Renderer.addFinishCamera
+     * e.g.: Renderer.addFinishCamera();
+     */
+    addFinishCamera() {
+        this.cameraFinish = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100000);
+        this.cameraFinish.position.set(10, 10, 10);
+        this.cameraFinish.lastPosition = new THREE.Vector3(10,10,10);
+        this.cameraFinish.lookAt(new THREE.Vector3(0,0,0));
+        this.scene.add(this.cameraFinish);
+    }
+    /**
+     * Renderer.addMultipleViewports
+     * e.g.: Renderer.addMultipleViewports();
+     */
+    addMultipleViewports() {
+        var _this = this;
+        this.views = [
+            {
+                left: 0, bottom: 0, width: 1.0, height: 1.0, eye: [ 10, 10, 10 ], up: [ 0, 1, 0 ],
+                updateCamera: function ( camera ) {
+                    camera.position = camera.lastPosition;
+                    camera.lookAt( camera.target );
+                }
+            }, {
+                left: 0, bottom: 0.6, width: 0.4, height: 0.4, eye: [ 0, 10, 0 ], up: [ 0, 0, 1 ],
+                updateCamera: function (camera) {
+                    camera.position.x = camera.position.x * Math.cos(0.1) + Math.sin(0.1);
+                    camera.position.z = camera.position.z * Math.cos(0.1) - Math.sin(0.1);
+                    camera.lookAt( _this.playerSnails.snails[_this.winner].position );
+                }
+            }
+        ];
+
+        this.views[0].camera = this.camera;
+        this.views[1].camera = this.cameraFinish;
     }
     /**
      * Renderer.addDirectionalLight
@@ -54,10 +121,51 @@ export class Renderer {
         this.scene.add(directionalLight);
     }
     /**
-     * Player.render
+     * Renderer.setRenderTarget
+     * e.g.: Renderer.setRenderTarget();
+     */
+    setRenderTarget() {
+        var container = document.createElement( 'div' );
+        document.body.appendChild( container );
+        container.id = 'viewport';
+        container.appendChild( this.webglRenderer.domElement );
+    }
+    /**
+     * Renderer.render
      * e.g.: Renderer.render();
      */
     render() {
+        if(this.isGameOver){
+            this.confetti.animate();
 
+            //viewports
+            for ( var k = 0; k < this.views.length; ++k ) {
+
+                this.camera = this.views[k].camera;
+                this.views[k].updateCamera( this.camera, this.scene);
+
+                var left   = Math.floor( window.innerWidth  * this.views[k].left );
+                var bottom = Math.floor( window.innerHeight * this.views[k].bottom );
+                var width  = Math.floor( window.innerWidth  * this.views[k].width );
+                var height = Math.floor( window.innerHeight * this.views[k].height );
+                this.webglRenderer.setViewport( left, bottom, width, height );
+                this.webglRenderer.setScissor( left, bottom, width, height );
+                this.webglRenderer.enableScissorTest ( true );
+
+                this.camera.aspect = width / height;
+                this.camera.updateProjectionMatrix();
+
+                this.webglRenderer.render(this.scene, this.camera);
+            }
+        }
+
+        if(!this.game.isGameOver) {
+            this.webglRenderer.render(this.scene, this.camera);
+        }
+
+        var _this = this;
+        requestAnimationFrame(function(){
+            _this.render();
+        });
     }
 }
--- src/modules/Game.js
+++ src/modules/Game.js
@@ -1,4 +1,3 @@
-import { PubSub } from './PubSub.js';
 import { Models } from './3d/Models.js';
 import { Environment } from './3d/Environment.js';
 import { Counter } from './3d/Counter.js';
@@ -8,7 +7,10 @@ import { Renderer } from './3d/Renderer.js';
 export class Game {
 
     constructor() {
-        this.pubsub = new PubSub();
+        this.scene = new THREE.Scene();
+        this.isGameOver = false;
+        this.winnerTrack = 0;
+        this.startTime;
 
         this.config = {
             trackWidth: 10 / 4,
@@ -19,86 +21,15 @@ export class Game {
             playerCount: 2
         };
 
-        this.isGameOver = false;
         this.playerSnails = {snails: []};
-        this.scene = new THREE.Scene();
-        this.startTime;
-        this.winner = 0;
         this.playerCount = this.config.playerCount;
 
-        this.renderer = new Renderer(this.scene);
+        this.renderer = new Renderer(this, this.scene);
         this.models = new Models({ scene: this.scene, playerSnails: this.playerSnails });
         this.environment = new Environment(this.scene, this.config);
         this.counter = new Counter(this.scene, function() {
             console.log('countdown callback');
         });
-
-        this.camera = new THREE.PerspectiveCamera(45,  window.innerWidth / window.innerHeight, 0.1, 100000);
-        this.cameraFinish = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 100000);
-        var _this = this;
-        this.views = [
-            {
-                left: 0,
-                bottom: 0,
-                width: 1.0,
-                height: 1.0,
-                eye: [ 10, 10, 10 ],//x,y,z position of camera
-                up: [ 0, 1, 0 ],//up vector
-                updateCamera: function ( camera ) {
-                    camera.position = camera.lastPosition;
-                    camera.lookAt( camera.target );
-                }
-            },
-            {
-                left: 0,
-                bottom: 0.6,
-                width: 0.4,
-                height: 0.4,
-                eye: [ 0, 10, 0 ],
-                up: [ 0, 0, 1 ],
-                updateCamera: function ( camera) {
-                    // camera.position.set(0,4, playerSnails[winner].position.z-10);
-
-                    camera.position.x = camera.position.x * Math.cos(0.1) + Math.sin(0.1);
-                    camera.position.z = camera.position.z * Math.cos(0.1) - Math.sin(0.1);
-
-                    camera.lookAt( _this.playerSnails.snails[_this.winner].position );
-                }
-            }
-        ];
-
-        this.init();
-    }
-
-    init(){
-
-        this.cameraFinish.position.set(10, 10, 10); // set position of the camera
-        this.cameraFinish.lastPosition = new THREE.Vector3(10,10,10);
-        this.cameraFinish.lookAt(new THREE.Vector3(0,0,0)); // scene point camera is looking at
-
-        this.scene.add(this.cameraFinish);
-
-        this.views[0].camera = this.camera;
-        this.views[1].camera = this.cameraFinish;
-
-        // set render target
-        var container = document.createElement( 'div' );
-        document.body.appendChild( container );
-        container.id = 'viewport';
-        container.appendChild( this.renderer.webglRenderer.domElement );
-
-        // create scene object, add fog to scene
-        this.scene.fog = new THREE.FogExp2("#c1e9e4", 0.01, 10);
-
-        // camera viewport and configuration, PerspectiveCamera(angle, aspect, near, far)
-        this.camera.position.set(10, 10, 10); // set position of the camera
-        this.camera.lastPosition = new THREE.Vector3(10,10,10);
-        this.camera.lookAt(new THREE.Vector3(0,0,0)); // scene point camera is looking at
-        this.scene.add(this.camera); // add camera to scene
-        //camera for finish screen
-
-        // handling window-resize, 100% height and 100% width
-        THREEx.WindowResize(this.renderer, this.camera);
     }
 
     getEndTime() {
@@ -121,11 +52,7 @@ export class Game {
     setCameraInGame(){
         var position = this.getFirstAndLastSnailPositionZ();
         var mid = (position.max + position.min) / 2;
-        this.camera.target = new THREE.Vector3(0,0,-mid);
-        this.camera.lastPosition = this.camera.position;
-        this.camera.lookAt(this.camera.target);
-        //camera.translateX(mid / 96);
-        this.camera.translateX(mid / 120);
+        this.renderer.updateInGameCamera(mid);
     }
     //draws slime for each snail
     addSlime(snailIndex){
@@ -180,42 +107,16 @@ export class Game {
             this.setGameOver(snailIndex);
     }
 
-    checkModelMove(e){
-        if (e.keyCode == 81 || e.which == 81){//81 =q
-            this.modelMove(0);
-        }
-        else if (e.keyCode == 67 || e.which == 67){// 67 = c
-            this.modelMove(1);
-        }
-        else if (e.keyCode == 78 || e.which == 78){// 78 = n
-            this.modelMove(2);
-        }
-        else if (e.keyCode == 80 || e.which == 80){// 80 = p
-            this.modelMove(3);
-        }
-    }
-
-    addUserInput(){
-        var _this = this;
-        window.addEventListener('keyup', _this.checkModelMove.bind(_this), false);
-    }
-
-    startGame(){
+    startGame(gameOverCallback){
         this.models.setPlayerSnails(this.playerCount);
-        this.render();
-    }
+        this.renderer.render();
 
-    removeControls() {
-        // keep movement for 3 seconds enabled
-        var _this = this;
-        setTimeout(function(){
-            window.removeEventListener('keyup', _this.checkModelMove.bind(_this), false);
-        }, 3000);
-    };
+        this.gameOverCallback = gameOverCallback;
+    }
 
     setGameOver(winID){
         this.isGameOver = true;
-        this.winner = winID;
+        this.winnerTrack = winID;
 
         this.environment.addWinnerCaption(1);
         this.confetti = new Confetti(this.scene, this.config, 1);
@@ -223,42 +124,7 @@ export class Game {
         //TODO: uncomment after moving camera
         //cameraFinish.position.set(1, 4, this.playerSnails.snails[winID].position.z - 8);
 
-        this.pubsub.publish('game:over', { endTime: this.getEndTime() });
+        this.endTime = this.getEndTime();
+        this.gameOverCallback();
     }
-
-    render(){
-
-        if(this.isGameOver){
-            this.confetti.animate();
-
-            //viewports
-            for ( var k = 0; k < this.views.length; ++k ) {
-
-                this.camera = this.views[k].camera;
-                this.views[k].updateCamera( this.camera, this.scene);
-
-                var left   = Math.floor( window.innerWidth  * this.views[k].left );
-                var bottom = Math.floor( window.innerHeight * this.views[k].bottom );
-                var width  = Math.floor( window.innerWidth  * this.views[k].width );
-                var height = Math.floor( window.innerHeight * this.views[k].height );
-                this.renderer.webglRenderer.setViewport( left, bottom, width, height );
-                this.renderer.webglRenderer.setScissor( left, bottom, width, height );
-                this.renderer.webglRenderer.enableScissorTest ( true );
-
-                this.camera.aspect = width / height;
-                this.camera.updateProjectionMatrix();
-
-                this.renderer.webglRenderer.render(this.scene, this.camera);
-            }
-        }
-
-        if(!this.isGameOver) {
-            this.renderer.webglRenderer.render(this.scene, this.camera);
-        }
-
-        var _this = this;
-        this.animationFrameID = requestAnimationFrame(function(){
-            _this.render();
-        });
-    };
 }
