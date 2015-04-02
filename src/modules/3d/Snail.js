82688f5c572da50267f2d7e8390f207ee21095ac
David Neubauer <davidneub@gmail.com>
2015-04-02 16:04:15 +0200

    reduced complexity of getSlime

--- src/modules/3d/Snail.js
+++ src/modules/3d/Snail.js
@@ -23,25 +23,18 @@ export class Snail extends Model {
         if(this.slimeCounter%20 == 0){
 
             var slimeTexture = THREE.ImageUtils.loadTexture('img/slime.png');
-            // set texture properties, repeat
             slimeTexture.wrapS = slimeTexture.wrapT = THREE.RepeatWrapping;
             slimeTexture.repeat.set(1, 1);
+
             var slimeTextureBegin = THREE.ImageUtils.loadTexture('img/slimeBegin.png');
-            // set texture properties, repeat
             slimeTextureBegin.wrapS = slimeTexture.wrapT = THREE.RepeatWrapping;
             slimeTextureBegin.repeat.set(1, 1);
 
-            if(this.slimeCounter != 0){
-                slime = new THREE.Mesh(
-                    new THREE.PlaneGeometry(0.9, 2, 1, 1),
-                    new THREE.MeshLambertMaterial({map: slimeTexture, transparent:true, alphaTest: 0.4})
-                );
-            } else {
-                slime = new THREE.Mesh(
-                    new THREE.PlaneGeometry(0.9, 2, 1, 1),
-                    new THREE.MeshLambertMaterial({map: slimeTextureBegin, transparent:true, alphaTest: 0.4})
-                );
-            }
+            var texture = this.slimeCounter != 0 ? slimeTexture : slimeTextureBegin;
+            slime = new THREE.Mesh(
+                new THREE.PlaneGeometry(0.9, 2, 1, 1),
+                new THREE.MeshLambertMaterial({map: texture, transparent:true, alphaTest: 0.4})
+            );
 
             slime.doubleSided = true;
             slime.receiveShadow = true;
