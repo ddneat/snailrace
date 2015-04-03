d5750c7244570bfec1d461c2fc11e97551a7cf5b
David Neubauer <davidneub@gmail.com>
2015-04-03 15:12:43 +0200

    extracted confetti init logic

--- src/modules/3d/Confetti.js
+++ src/modules/3d/Confetti.js
@@ -21,31 +21,62 @@ export class Confetti {
      * e.g.: Confetti.init();
      */
     init() {
-        var materials = [];
-        var positionX = this.config.trackWidth * (this.track - 1) + 1.3;
-
         for (var i = 0; i < 15; i++) {
-            var geometry = new THREE.Geometry();
-
-            for ( var j = 0; j < 1200; j ++ ) {
-                var vertex = new THREE.Vector3();
-                vertex.x = Math.random() * 2 - 1;
-                vertex.y = Math.random() * 15 - 1;
-                vertex.z = Math.random() * 30 - 1;
-                vertex.velocity = new THREE.Vector3(0, -1, 0);
-                geometry.vertices.push( vertex );
-            }
-
-            var color = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-            materials[i] = new THREE.ParticleBasicMaterial( { size: 0.1 } );
-            materials[i].color = new THREE.Color(color);
-
-            this.particles[i] = new THREE.ParticleSystem( geometry, materials[i] );
-            this.particles[i].position.set(positionX, 1, -26);
+            this.particles[i] = new THREE.ParticleSystem(this.getParticleGeometry(), this.getParticleMaterial());
+            this.particles[i].position.set(this.getTrackPositionX(), 1, -26);
             this.particles[i].sortPosition = true;
 
             this.scene.add(this.particles[i]);
         }
+    }
+    /**
+     * Confetti.getTrackPositionX
+     * e.g.: Confetti.getTrackPositionX();
+     *
+     * @return {Number}
+     */
+    getTrackPositionX() {
+        return this.config.trackWidth * (this.track - 1) + 1.3;
+    }
+    /**
+     * Confetti.getParticleGeometry
+     * e.g.: Confetti.getParticleGeometry();
+     *
+     * @return {Object}
+     */
+    getParticleGeometry() {
+        var geometry = new THREE.Geometry();
+
+        for ( var j = 0; j < 1200; j ++ ) {
+            var vertex = new THREE.Vector3();
+            vertex.x = Math.random() * 2 - 1;
+            vertex.y = Math.random() * 15 - 1;
+            vertex.z = Math.random() * 30 - 1;
+            vertex.velocity = new THREE.Vector3(0, -1, 0);
+            geometry.vertices.push( vertex );
+        }
+
+        return geometry;
+    }
+    /**
+     * Confetti.getParticleMaterial
+     * e.g.: Confetti.getParticleMaterial();
+     *
+     * @return {Object}
+     */
+    getParticleMaterial() {
+        var material = new THREE.ParticleBasicMaterial( { size: 0.1 } );
+        material.color = new THREE.Color(this.getRandomColor());
+        return material;
+    }
+    /**
+     * Confetti.getRandomColor
+     * e.g.: Confetti.getRandomColor();
+     *
+     * return {String} #000000
+     */
+    getRandomColor() {
+        return "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);})
     }
     /**
      * Confetti.animate
